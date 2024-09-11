f7ec4f475ee68c7d4d7e64ff91ba0e83364fdedb
Alan Da Silva <luckywall80@gmail.com>
2024-09-10 21:22:11 -0300

    fudamentos-002

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-// Olá, Mundo!
-const mensagem = "Olá, Mundo!"
-console.log(mensagem)
-console.log(mensagem)
-console.log(mensagem)
-console.log(mensagem)
+// arrays, objetos 
+let array = ["Maiky", "Alan"]
+
+console.log(array[0] + "," + array[1])
+
+let objeto = {
+    value: 'ler um livro por mês',
+    checked: false,
+    isChecked: () => {}
+}
+
+console.log(objeto.isChecked())
+
+//function // arrow function
+
+const criarMeta = () => {}
+
+//function
+
+function criarMetas(){
+
+}
