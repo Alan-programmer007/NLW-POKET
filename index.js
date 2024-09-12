41beb0e990429ce9edd32c736f743b7e7128595e
Alan Da Silva <luckywall80@gmail.com>
2024-09-12 20:33:16 -0300

    metas abertas

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const listarMetas = async () => {
     console.log("Meta(s) marcas como concluída(s)")
 }
 
-const metasRealizadas = async () =>{
+const metasRealizadas = async () => {
     const realizadas = metas.filter((meta) => {
         return meta.checked
     })
@@ -58,8 +58,22 @@ const metasRealizadas = async () =>{
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas" + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+    if (abertas.length == 0) {
+        console.log("Não existem metas em aberto :)")
+        return
+    }
+    await select({
+        message: "Metas em aberto " + metas.length,
+        choices: [...abertas]
     })
 }
 
@@ -83,6 +97,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -100,6 +118,9 @@ const start = async () => {
                 break;
             case "realizadas":
                 await metasRealizadas()
+            break
+            case "abertas":
+                await metasAbertas()
             break
             case "sair":
                 console.log("Até a próxima")
